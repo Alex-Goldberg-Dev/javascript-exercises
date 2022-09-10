a98da121acc6e8e497b83a64032dec4f394ebaf3
Alex-Goldberg-Dev <alex.h.goldberg16@gmail.com>
2022-09-10 17:07:24 -0400

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let result = [];
+    array.forEach(val => {
+        if (!args.includes(val)) result.push(val);
+    })
+    return result;
 };
 
 // Do not edit below this line
