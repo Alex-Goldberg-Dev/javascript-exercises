2cf6e6ed96cfe54b2266b477bf20136dc08c8e4a
Alex-Goldberg-Dev <alex.h.goldberg16@gmail.com>
2022-09-10 17:29:13 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+    let result = 0;
+    let lowNum;
+    let highNum;
+    if (num1 - num2 > 0) {
+        highNum = num1;
+        lowNum = num2;
+    } else {
+        highNum = num2;
+        lowNum = num1;
+    }
+    for (let i = lowNum; i <= highNum; i++) result += i;
+    return result;
 };
 
 // Do not edit below this line
