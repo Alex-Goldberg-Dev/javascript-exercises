8a93f6782fcfe517392b3f6f547e71e2d85d242f
Alex-Goldberg-Dev <alex.h.goldberg16@gmail.com>
2022-09-10 17:36:01 -0400

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap years are divisible by 4 except years that divisible by 100 unless also divisible by 400
+    if (year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else if (year % 4 === 0) return true;
+    else return false;
 };
 
 // Do not edit below this line
